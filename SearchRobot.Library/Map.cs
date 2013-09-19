8667b93195a582bb1eafc23930354d1903a16673
sc0238 <christoph.seiler@gmail.com>
2013-09-19 20:35:42 +0200

    Map with prototype save and load.

---
+++ SearchRobot.Library/Map.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchRobot.Library
+{
+    /// <summary>
+    /// Represents the underlying Map.
+    /// </summary>
+    public class Map
+    {
+        private readonly int[][] _map;
+
+
+        /// <summary>
+        /// Loads a Map from a stream.
+        /// </summary>
+        /// <param name="stream">Stream as source of the map.</param>
+        /// <returns>Loaded Map.</returns>
+        public static Map Load(Stream stream)
+        {
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                reader.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Writes the map structure to a stream.
+        /// </summary>
+        /// <param name="stream">Stream to be used to save the Map.</param>
+        public void Save(Stream stream)
+        {
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                foreach (int[] column in _map)
+                {
+                    foreach (int val in column)
+                    {
+                        writer.Write(val);
+                    }
+
+                    writer.WriteLine();
+                }
+            }
+        }
+
+    }
+}
