136d764092302f4394a57308786ae6441819d07a
sc0238 <christoph.seiler@gmail.com>
2013-10-18 10:21:09 +0200

    Save & Load Implementation.

--- SearchRobot.Library/Maps/Map.cs
+++ SearchRobot.Library/Maps/Map.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SearchRobot.Library.Maps
 {
-    public class Map
-    {
-        private readonly List<MapElement> _mapElements;
+	public class Map
+	{
+		private readonly List<MapElement> _mapElements;
  
-        public Map()
-        {
-            _mapElements = new List<MapElement>();
-        }
+		public Map()
+		{
+			_mapElements = new List<MapElement>();
+		}
 
-        public IReadOnlyCollection<MapElement> Elements { get { return _mapElements.AsReadOnly(); } }
+		public IReadOnlyCollection<MapElement> Elements { get { return _mapElements.AsReadOnly(); } }
  
-        public void Add(MapElement mapElement)
-        {
-            _mapElements.Add(mapElement);
-        }
+		public void Add(MapElement mapElement)
+		{
+			_mapElements.Add(mapElement);
+		}
 
-        public void Remove(MapElement mapElement)
-        {
-            _mapElements.Remove(mapElement);
-        }
-    }
+		public void Remove(MapElement mapElement)
+		{
+			_mapElements.Remove(mapElement);
+		}
+
+		public void ApplyToCanvas(Canvas canvas)
+		{
+			foreach (var mapelement in Elements)
+			{
+				mapelement.ApplyTo(canvas);
+			}
+		}
+	}
 }
--- SearchRobot.Library/Maps/Wall.cs
+++ SearchRobot.Library/Maps/Wall.cs
@@ -12,23 +12,17 @@ namespace SearchRobot.Library.Maps
 {
     public class Wall : MapElement
     {
-        private Line _uiElement;
+        private Rectangle _uiElement;
 	    private Geometry _geometry;
 
-		private double GetAngle(Point start, Point end)
-		{
-			var y = end.Y - start.Y;
-			var x = end.X - start.X;
-
-			return Math.Atan2(y, x) / Math.PI * 180;
-		}
+	    private const int height = 4;
 
 		public Geometry AsRectangleGeometry(Point start, Point end)
 		{
-			var angle = GetAngle(start, end);
-			var width = Math.Sqrt(Math.Pow(Math.Abs(start.X - end.X), 2) + Math.Pow(Math.Abs(start.Y - end.Y), 2));
+			var angle = GeometryHelper.GetAngle(start, end);
+			var width = GeometryHelper.GetWidth(start, end);
 
-			return new RectangleGeometry(new Rect(start.X, start.Y, width, 2), 0, 0, new RotateTransform(angle));
+			return new RectangleGeometry(new Rect(start.X, start.Y, width, height), 0, 0, new RotateTransform(angle));
 		}
 
         public Wall(Map map) : base(map)
@@ -43,17 +37,12 @@ namespace SearchRobot.Library.Maps
 		    get
 		    {
 			    return AsRectangleGeometry(StartPosition, EndPoint);
-
-			    return new LineGeometry(
-					new System.Windows.Point(StartPosition.X, StartPosition.Y),
-					new System.Windows.Point(EndPoint.X, EndPoint.Y));
 		    }
 	    }
 
 	    public override void MouseDown(Canvas canvas, Point point)
         {
             StartPosition = EndPoint = point;
-
 			ApplyTo(canvas);
         }
 
@@ -64,30 +53,32 @@ namespace SearchRobot.Library.Maps
 
         public override void MouseMove(Canvas canvas, Point point)
         {
-            _uiElement.X2 = point.X;
-            _uiElement.Y2 = point.Y;
+	        EndPoint = point;
+
+	        _uiElement.Width = GeometryHelper.GetWidth(StartPosition, EndPoint);
+			_uiElement.RenderTransform = new RotateTransform(GeometryHelper.GetAngle(StartPosition, EndPoint));
 
 	        bool isOverlapping = IsOverlapping();
 
+
 	        _uiElement.Fill = isOverlapping ? Brushes.Red : Brushes.Black;
-			_uiElement.Stroke = new SolidColorBrush(IsOverlapping() ? Color.FromRgb(255,0,0) : Color.FromRgb(0,0,0));
         }
 
-	    public override void ApplyTo(Canvas canvas)
-	    {
-			_uiElement = new Line
-				{
-					Fill = Brushes.Black,
-					StrokeThickness = 2,
-					Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
-					X1 = StartPosition.X,
-					Y1 = StartPosition.Y,
-					X2 = EndPoint.X,
-					Y2 = EndPoint.Y
-				};
+		public override void ApplyTo(Canvas canvas)
+		{
+			_uiElement = new Rectangle { Height = height, Fill = Brushes.Black };
 
-		    canvas.Children.Add(_uiElement);
-	    }
+			_uiElement.Width = GeometryHelper.GetWidth(StartPosition, EndPoint);
+			_uiElement.RenderTransform = new RotateTransform(
+												GeometryHelper.GetAngle(StartPosition, EndPoint),
+												StartPosition.X,
+												StartPosition.Y);
+
+			Canvas.SetLeft(_uiElement, StartPosition.X);
+			Canvas.SetTop(_uiElement, StartPosition.Y);
+
+			canvas.Children.Add(_uiElement);
+		}
 
 	    public override void Remove(Canvas canvas)
 	    {
